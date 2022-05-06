7cec7a6b06d4cffde37effe2602a6f0c59fd9b01
Pratik <itspratik360@gmail.com>
2022-05-06 23:32:26 +0530

    Adding Two Sum Quetion

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,36 @@
+// this problem can be solved by O(n) time complexity if you make use of Map
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+            Map<Integer,Integer> map = new HashMap<>();
+            for(int i =0; i< nums.length; i++) {
+                if(map.containsKey(target - nums[i])){
+                    return new int[]{map.get(target - nums[i]), i};
+                }else {
+                    map.put(nums[i], i);
+                }
+            }
+            return new int[]{-1,-1};
+        }
+    }
+
+    //  MY OWN METHOD
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int complement; 
+//         //loop to check every element in the array
+//         for (int x = 0; x<nums.length; x++) {  
+//             complement = target - nums[x];
+//             //loop to find complement of current element
+//             for (int y = 0; y<nums.length; y++) { 
+//                 //we cannot use same element twice.
+//                 if (x ==  y) { continue; } 
+//                 if (nums[y] == complement) {
+//                     return new int[] {x, y};
+//                 }
+//             }            
+//         }
+//         return new int[] {0, 0};
+//     }
+// }
