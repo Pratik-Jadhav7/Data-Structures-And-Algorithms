f2718a4ed79524d50eab2f7d9edfc13132e6dba5
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-05-20 23:33:05 +0530

    Add files via upload

---
+++ 217. Contains Duplicate/Answer.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i < nums.length; i++){
+            if(nums[i-1]==nums[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
