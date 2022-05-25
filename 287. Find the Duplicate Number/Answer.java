d02f186d8e3848c56e101ecc4557fcf2e9602dcb
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-05-25 23:48:52 +0530

    Add files via upload

---
+++ 287. Find the Duplicate Number/Answer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i , correct);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
