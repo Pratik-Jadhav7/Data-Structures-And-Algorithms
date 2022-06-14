e53129f29bbaa8c053a1fbd001ae55266144f77f
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-06-14 22:26:15 +0530

    Add files via upload

---
+++ 1512. Number of Good Pairs/Quetion.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+    // initialise variable for return the number
+      int p = 0;
+        
+        // Run outer for loop i
+        for(int i=0; i<nums.length-1; i++){
+            
+            // Run inner for loop for j
+            for(int j=i+1; j<nums.length; j++){
+                
+                // Apply if condition
+                if(nums[i] == nums[j]) p++;
+            }
+        }
+        return p;
+    }
+}
