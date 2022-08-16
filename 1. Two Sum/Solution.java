78859ed405ee38714ee6f09df99cc7b7c5bc28c4
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-08-16 23:57:41 +0530

    Update Solution.java

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -14,23 +14,23 @@ class Solution {
         }
     }
 
-    //  MY OWN METHOD
+     MY OWN METHOD
 
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         int complement; 
-//         //loop to check every element in the array
-//         for (int x = 0; x<nums.length; x++) {  
-//             complement = target - nums[x];
-//             //loop to find complement of current element
-//             for (int y = 0; y<nums.length; y++) { 
-//                 //we cannot use same element twice.
-//                 if (x ==  y) { continue; } 
-//                 if (nums[y] == complement) {
-//                     return new int[] {x, y};
-//                 }
-//             }            
-//         }
-//         return new int[] {0, 0};
-//     }
-// }
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int complement; 
+        //loop to check every element in the array
+        for (int x = 0; x<nums.length; x++) {  
+            complement = target - nums[x];
+            //loop to find complement of current element
+            for (int y = 0; y<nums.length; y++) { 
+                //we cannot use same element twice.
+                if (x ==  y) { continue; } 
+                if (nums[y] == complement) {
+                    return new int[] {x, y};
+                }
+            }            
+        }
+        return new int[] {0, 0};
+    }
+}
