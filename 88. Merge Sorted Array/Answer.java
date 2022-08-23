2abc481505dc09434d354442874f17108522d137
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-08-23 20:10:31 +0530

    Update Answer.java

--- 88. Merge Sorted Array/Answer.java
+++ 88. Merge Sorted Array/Answer.java
@@ -4,23 +4,23 @@
         
 //         while(m>0 && n>0){
             
-// //             starting from last index
-//             if(nums1[m-1]<nums2[n-1]){
-//                 nums1[index--]= nums2[--n];
-//             } else {
-//                 nums1[index--] = nums1[--m];
-//             }
-//         }
+//             starting from last index
+            if(nums1[m-1]<nums2[n-1]){
+                nums1[index--]= nums2[--n];
+            } else {
+                nums1[index--] = nums1[--m];
+            }
+        }
         
-// //         all remaining nums1 values
-//             while(m>0){
-//                 nums1[index--]= nums1[--m];
-//             }
+//         all remaining nums1 values
+            while(m>0){
+                nums1[index--]= nums1[--m];
+            }
         
-// //         all remianing nums2 values
-//             while(n>0){
-//                 nums1[index--]= nums2[--n];
-//             }
-//         }
+//         all remianing nums2 values
+            while(n>0){
+                nums1[index--]= nums2[--n];
+            }
+        }
         
-//     }
+    }
