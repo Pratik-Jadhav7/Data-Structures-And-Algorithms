62c0ab07588eb3deb48644a93ac11f74f67659a6
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-08-23 20:11:41 +0530

    Update Answer.java

--- 88. Merge Sorted Array/Answer.java
+++ 88. Merge Sorted Array/Answer.java
@@ -1,8 +1,8 @@
-// class Solution {
-//     public void merge(int[] nums1, int m, int[] nums2, int n) {
-//         int index = nums1.length - 1;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = nums1.length - 1;
         
-//         while(m>0 && n>0){
+        while(m>0 && n>0){
             
 //             starting from last index
             if(nums1[m-1]<nums2[n-1]){
