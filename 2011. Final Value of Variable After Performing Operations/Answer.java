11752830d40b6f40403e3b3d96c409af81f25088
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-08-31 23:22:03 +0530

    Update Answer.java

--- 2011. Final Value of Variable After Performing Operations/Answer.java
+++ 2011. Final Value of Variable After Performing Operations/Answer.java
@@ -1,14 +1,14 @@
-class Solution {
-    public int finalValueAfterOperations(String[] operations) {
-        int X=0;
-        for(int i=0;i<=operations.length-1;i++){
-            if(operations[i].equals("++X") || operations[i].equals("X++")){
-                X++;
-            }
-            if(operations[i].equals("--X") || operations[i].equals("X--")){
-                X--;
-            }
-        }
-        return X;
-    }
-}
+// class Solution {
+//     public int finalValueAfterOperations(String[] operations) {
+//         int X=0;
+//         for(int i=0;i<=operations.length-1;i++){
+//             if(operations[i].equals("++X") || operations[i].equals("X++")){
+//                 X++;
+//             }
+//             if(operations[i].equals("--X") || operations[i].equals("X--")){
+//                 X--;
+//             }
+//         }
+//         return X;
+//     }
+// }
