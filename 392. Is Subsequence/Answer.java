54b8c6c36b74150180d61cf14071684ca311646d
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-09-09 21:44:46 +0530

    Update Answer.java

--- 392. Is Subsequence/Answer.java
+++ 392. Is Subsequence/Answer.java
@@ -10,3 +10,34 @@ class Solution {
 	return i == s.length();
 }
 }
+
+
+
+
+
+
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+ 
+    if(s.length() == 0){
+        return true;
+    }
+       int s_pointer = 0;
+       int t_pointer = 0;
+        
+        while(t_pointer < t.length()){
+            if(t.charAt(t_pointer) == s.charAt(s_pointer)){
+                
+                s_pointer++;
+                
+                if(s_pointer == s.length()){
+                    return true;
+                }
+            }
+            t_pointer++;
+                
+        }
+        return false;
+    }
+}
