87698551ee812c0369dabac5ab5be34db74abdc7
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-08 22:33:05 +0530

    Time: 5 ms (31.32%), Space: 41.9 MB (64.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(c == '(' || c== '[' || c== '{')
+                stack.push(c);
+            
+            else{ 
+                if(stack.isEmpty()){
+                return false;
+            }
+            if(c == ')'){
+                if('(' != stack.pop())
+                    return false;
+            } 
+           else if(c==']'){
+                if('[' != stack.pop())
+                    return false;
+            } else if(c == '}'){
+                if('{' != stack.pop())
+                    return false;
+            }
+        }
+            }
+            return stack.isEmpty();
+    }
+}
