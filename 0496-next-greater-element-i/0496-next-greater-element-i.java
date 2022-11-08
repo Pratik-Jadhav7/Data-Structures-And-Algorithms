3e862c1434b2cf8120ec3d0dc0f8834fbe71b0fe
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-08 20:05:31 +0530

    Update 0496-next-greater-element-i.java

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,3 +1,33 @@
+Brute
+Time - O(n^2)
+
+Code
+
+public int[] nextGreaterElementBrute(int[] nums1, int[] nums2) {
+	int[] ans = new int[nums1.length];
+
+	for(int i = 0; i < nums1.length; i++) {
+		int greaterIdx = -1, j = nums2.length - 1;
+		while(j >= 0 && nums2[j] != nums1[i]) {
+			if(nums2[j] > nums1[i]){
+				greaterIdx = nums2[j];
+			}
+			j--;
+		}
+		ans[i] = greaterIdx;
+	}
+
+	return ans;
+}
+
+
+
+
+Optimal
+Time - O(n) 
+
+Code
+
 class Solution {
        public int[] nextGreaterElement(int[] findNums, int[] nums) {
         Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
