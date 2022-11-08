4bbd81bec3f1b0aca5d9b7a3214d175d5b6c7a42
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-08 19:49:34 +0530

    Time: 14 ms (84.31%), Space: 55.7 MB (6.50%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,11 +1,22 @@
+public class Solution {
 
- class Solution {
-    public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int result = 0;
-        for (int i = 0; i < nums.length; i += 2) {
-            result += nums[i];
-        }
-        return result;
-    }
+	public int arrayPairSum(int[] nums) {
+		int[] exist = new int[20001];
+		for (int i = 0; i < nums.length; i++) {
+			exist[nums[i] + 10000]++;
+		}
+		int sum = 0;
+		boolean odd = true;
+		for (int i = 0; i < exist.length; i++) {
+			while (exist[i] > 0) {
+				if (odd) {
+					sum += i - 10000;
+				}
+				odd = !odd;
+				exist[i]--;
+			}
+		}
+		return sum;
+	}
 }
+	
