3700a575dfb264bd8ed40c50bba54850631bddc2
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-12 16:23:11 +0530

    Time: 0 ms (100.00%), Space: 49.8 MB (31.67%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,32 +14,36 @@
  * }
  */
 class Solution {
-  public TreeNode deleteNode(TreeNode root, int key) {
-    if(root == null){
-        return null;
-    }
-    if(key < root.val){
-        root.left = deleteNode(root.left, key);
-    }else if(key > root.val){
-        root.right = deleteNode(root.right, key);
-    }else{
-        if(root.left == null){
-            return root.right;
-        }else if(root.right == null){
-            return root.left;
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
+        
+        if(key<root.val){                            
+            root.left = deleteNode(root.left,key);
+            return root;
         }
         
-        TreeNode minNode = findMin(root.right);
-        root.val = minNode.val;
-        root.right = deleteNode(root.right, root.val);
+        else if(key>root.val){
+            root.right = deleteNode(root.right,key);
+            return root;
+        }
+        
+        else{
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            else{
+                TreeNode min = root.right;
+                while(min.left!=null){
+                    min = min.left;
+                }
+                
+                root.val = min.val;
+                root.right = deleteNode(root.right,min.val);
+                return root;
+            }
+        }
     }
-    return root;
-}
-
-private TreeNode findMin(TreeNode node){
-    while(node.left != null){
-        node = node.left;
-    }
-    return node;
-}
 }
