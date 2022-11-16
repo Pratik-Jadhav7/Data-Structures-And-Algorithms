e0ea6e7c9365290c8fcb648fd6a540d2a49415a5
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-16 23:42:27 +0530

    Time: 3 ms (67.97%), Space: 104.6 MB (40.34%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -8,24 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// Approach
+
+// Assign left pointer as list1 and traverse till you find a value
+// Have middle pointer and traverse till you find b value
+// Map the left to list2 and traverse till the end
+// At last assign the next element of the list2 as the next element of the middle pointer
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        ListNode start = list1;
-        for (int i = 1; i < a; i++) {
-            start = start.next;
-        }
+        ListNode left = list1;
+        for (int i = 1; i < a; i++)
+            left = left.next;
         
-        ListNode end = start;
-        for (int i = a; i <= b; i++) {
-            end = end.next;
-        }
+        ListNode middle = left;
+        for (int i = a; i <= b; i++)
+            middle = middle.next;
         
-        start.next = list2;
-        while (list2.next != null) {
+		left.next = list2;
+        while (list2.next != null)
             list2 = list2.next;
-        }
         
-        list2.next = end.next;
+        list2.next = middle.next;
         return list1;
     }
 }
