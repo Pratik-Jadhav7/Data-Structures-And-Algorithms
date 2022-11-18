525e11eb5590707db84a51bff24230dcfe978f89
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-18 21:42:17 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (57.54%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,18 +14,54 @@
  * }
  */
 class Solution {
-
-public List<Integer> inorderTraversal(TreeNode root) {
-    List<Integer> res = new ArrayList<Integer>();
-    in(root,res);
-    return res;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        solve(res,root);
+        return res;
+    }
+    
+    private void solve(List<Integer> res, TreeNode root){
+        if(root == null) return;
+        
+		// Traverse left node
+        solve(res, root.left);
+		
+		// Traverse parent node
+        res.add(root.val);
+		
+		// Traverse right node
+        solve(res, root.right);
+    }
 }
 
-public void in(TreeNode root,List<Integer> res){
-    if(root==null)
-        return;
-    in(root.left,res);
-    res.add(root.val);
-    in(root.right,res);
-}
-}
+
+
+// Using Stack
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+        
+//         TreeNode curr = root;
+//         Stack<TreeNode> stack = new Stack<>();
+//         while(curr != null || stack.size() != 0){
+            
+//             // Move to the left-most node of the tree and push them in stack for future traversal
+//             while(curr != null){
+//                 stack.push(curr);
+//                 curr = curr.left;
+//             }
+            
+//             // Left node
+//             curr = stack.pop();
+            
+//             // Parent node
+//             res.add(curr.val);
+            
+//             // Right node
+//             curr = curr.right;
+//         }
+        
+//         return res;
+//     }
+// }
