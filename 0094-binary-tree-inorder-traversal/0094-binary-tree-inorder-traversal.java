ce1d76357fad8644f07e1b2300e2d0d96b690253
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-18 21:42:41 +0530

    Time: 1 ms (47.41%), Space: 40.7 MB (83.10%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,55 +13,55 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        solve(res,root);
-        return res;
-    }
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         solve(res,root);
+//         return res;
+//     }
     
-    private void solve(List<Integer> res, TreeNode root){
-        if(root == null) return;
+//     private void solve(List<Integer> res, TreeNode root){
+//         if(root == null) return;
         
-		// Traverse left node
-        solve(res, root.left);
+// 		// Traverse left node
+//         solve(res, root.left);
 		
-		// Traverse parent node
-        res.add(root.val);
+// 		// Traverse parent node
+//         res.add(root.val);
 		
-		// Traverse right node
-        solve(res, root.right);
-    }
-}
+// 		// Traverse right node
+//         solve(res, root.right);
+//     }
+// }
 
 
 
 // Using Stack
 
-// class Solution {
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         List<Integer> res = new ArrayList<>();
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
         
-//         TreeNode curr = root;
-//         Stack<TreeNode> stack = new Stack<>();
-//         while(curr != null || stack.size() != 0){
+        TreeNode curr = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while(curr != null || stack.size() != 0){
             
-//             // Move to the left-most node of the tree and push them in stack for future traversal
-//             while(curr != null){
-//                 stack.push(curr);
-//                 curr = curr.left;
-//             }
+            // Move to the left-most node of the tree and push them in stack for future traversal
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
             
-//             // Left node
-//             curr = stack.pop();
+            // Left node
+            curr = stack.pop();
             
-//             // Parent node
-//             res.add(curr.val);
+            // Parent node
+            res.add(curr.val);
             
-//             // Right node
-//             curr = curr.right;
-//         }
+            // Right node
+            curr = curr.right;
+        }
         
-//         return res;
-//     }
-// }
+        return res;
+    }
+}
