ed0e5fd88f987a9b846c20a2a19a6faa4c527a12
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-19 23:10:06 +0530

    Time: 1 ms (8.36%), Space: 41.8 MB (29.52%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,11 +14,34 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-    if(p == null && q == null) return true;
-    if(p == null || q == null) return false;
-    if(p.val == q.val)
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    return false;
+   public boolean isSameTree(TreeNode p, TreeNode q) {
+    // iteration method
+    if (p == null && q == null) return true;
+    if (p == null && q != null || p != null && q == null) return false;
+    Stack<TreeNode> stackP = new Stack<>();
+    Stack<TreeNode> stackQ = new Stack<>();
+    stackP.add(p);
+    stackQ.add(q);
+    while (!stackP.isEmpty() && !stackQ.isEmpty()) {
+        TreeNode tmpP = stackP.pop();
+        TreeNode tmpQ = stackQ.pop();
+        if (tmpP.val != tmpQ.val) return false;
+        if (tmpP.left != null && tmpQ.left != null) {
+            stackP.push(tmpP.left);
+            stackQ.push(tmpQ.left);
+        } else if (tmpP.left == null && tmpQ.left == null) {
+        } else {
+            return false;
+        }
+        if (tmpP.right != null && tmpQ.right != null) {
+            stackP.push(tmpP.right);
+            stackQ.push(tmpQ.right);
+        } else if (tmpP.right == null && tmpQ.right == null) {
+        } else {
+            return false;
+        }
+    }
+    if (!stackP.isEmpty() || !stackQ.isEmpty()) return false;
+    return true;
 }
 }
