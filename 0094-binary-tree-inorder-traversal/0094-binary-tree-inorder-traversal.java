c0744034810baae81e359c93e2e0a4dd91100e0f
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-22 08:36:43 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (57.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,55 +13,25 @@
  *     }
  * }
  */
-// class Solution {
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         List<Integer> res = new ArrayList<>();
-//         solve(res,root);
-//         return res;
-//     }
-    
-//     private void solve(List<Integer> res, TreeNode root){
-//         if(root == null) return;
-        
-// 		// Traverse left node
-//         solve(res, root.left);
-		
-// 		// Traverse parent node
-//         res.add(root.val);
-		
-// 		// Traverse right node
-//         solve(res, root.right);
-//     }
-// }
-
-
-
-// Using Stack
-
 class Solution {
+    List<Integer> list = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        
-        TreeNode curr = root;
-        Stack<TreeNode> stack = new Stack<>();
-        while(curr != null || stack.size() != 0){
-            
-            // Move to the left-most node of the tree and push them in stack for future traversal
-            while(curr != null){
-                stack.push(curr);
-                curr = curr.left;
-            }
-            
-            // Left node
-            curr = stack.pop();
-            
-            // Parent node
-            res.add(curr.val);
-            
-            // Right node
-            curr = curr.right;
+        if(root==null){
+            return list;
         }
+        inorder(root);
+        return list;
+    }
+    
+    public void inorder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
         
-        return res;
+        return 
+            ;
     }
 }
