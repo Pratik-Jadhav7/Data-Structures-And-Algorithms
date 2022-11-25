f18328b2bdce3b02512e064165afb7d51a849606
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-25 20:09:21 +0530

    Time: 3 ms (5.33%), Space: 51.5 MB (10.07%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -14,20 +14,37 @@
  * }
  */
 class Solution {
-   // Method 1: Recursive Solution
+// Method 2: Iterative DFS
 // Time: O(n)
 // Space: O(height)
 public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
   if (t1 == null) {
     return t2;
   }
-
-  if (t2 != null) {
-    t1.val += t2.val;
-    t1.left = mergeTrees(t1.left, t2.left);
-    t1.right = mergeTrees(t1.right, t2.right);
+  // Use stack to help DFS
+  Deque<TreeNode[]> stack = new LinkedList<>();
+  stack.offerLast(new TreeNode[] {t1, t2});
+  while (!stack.isEmpty()) {
+    TreeNode[] cur = stack.pollLast();
+    // no need to merge t2 into t1
+    if (cur[1] == null) {
+      continue;
+    }
+    // merge t1 and t2
+    cur[0].val += cur[1].val;
+    // if node in t1 == null, use node in t2 instead
+    // else put both nodes in stack to merge
+    if (cur[0].left == null) {
+      cur[0].left = cur[1].left;
+    } else {
+      stack.offerLast(new TreeNode[] {cur[0].left, cur[1].left});
+    }
+    if (cur[0].right == null) {
+      cur[0].right = cur[1].right;
+    } else {
+      stack.offerLast(new TreeNode[] {cur[0].right, cur[1].right});
+    }
   }
-
   return t1;
 }
 }
