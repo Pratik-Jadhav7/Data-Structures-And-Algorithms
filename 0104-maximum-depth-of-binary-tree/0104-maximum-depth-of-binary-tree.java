18beda9710f38c5087fc8d14e68bc8a0295b2161
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-26 11:23:04 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (36.26%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,10 +19,7 @@ class Solution {
             return 0;
         }
         
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        
-        return Math.max(left, right)+1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
         
     }
 }
