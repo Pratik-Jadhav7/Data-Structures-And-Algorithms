5680ca12997ae882203e7760310def52e4f86e85
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-26 11:59:00 +0530

    Time: 1 ms (99.19%), Space: 44.4 MB (38.46%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,21 +13,32 @@
  *     }
  * }
  */
-public class Solution {
-private boolean result = true;
-
-public boolean isBalanced(TreeNode root) {
-    maxDepth(root);
-    return result;
-}
-
-public int maxDepth(TreeNode root) {
-    if (root == null)
-        return 0;
-    int l = maxDepth(root.left);
-    int r = maxDepth(root.right);
-    if (Math.abs(l - r) > 1)
-        result = false;
-    return 1 + Math.max(l, r);
-}
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        if(height(root)==-1){
+            return false;
+        }
+        return true;
+    }
+    
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        
+        int left = height(root.left);
+        int right = height(root.right);
+        
+        if(left == -1 || right == -1){
+            return -1;
+        }
+        if(Math.abs(left-right)>1){
+            return -1;
+        }
+        
+        return Math.max(left, right)+1;
+    }
 }
