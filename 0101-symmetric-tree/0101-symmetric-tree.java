39be6889ff9ca25608a9aa8393f0e699c6bea0eb
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-30 15:28:59 +0530

    Time: 1 ms (77.53%), Space: 40.7 MB (85.78%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,23 +14,30 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if(root == null) return false;
-        
-       Stack<TreeNode> lStack = new Stack<>();
-       Stack<TreeNode> rStack = new Stack<>();
-        lStack.push(root.left); rStack.push(root.right);
-        
-          while (lStack.size() > 0 && rStack.size() > 0)  {
-                TreeNode t1 = lStack.pop();
-    TreeNode t2 = rStack.pop();
-               if (t1 == null && t2 == null) continue;
+  public boolean isSymmetric(TreeNode root) {
+  if (root == null) {
+    return true;
+  }
+  if (root.left == null && root.right == null) return true;
+  if (root.left == null || root.right == null) return false;
+  // children are not null
+  Stack<TreeNode> stack = new Stack<>();
+  stack.push(root.left);
+  stack.push(root.right);
+  
+  while (stack.size() > 0) {
+    TreeNode t1 = stack.pop();
+    TreeNode t2 = stack.pop();
+    // null check
+    if (t1 == null && t2 == null) continue;
     if (t1 == null || t2 == null) return false;
-                if (t1.val != t2.val) return false;
-               lStack.push(t1.right); lStack.push(t1.left); // could be null
-    rStack.push(t2.left);  rStack.push(t2.right);
-    }
-  // One of the stack might be empty
-  return lStack.size() == 0 && rStack.size() == 0;
+    // value check
+    if (t1.val != t2.val) return false;
+    // push children
+    stack.push(t1.right); stack.push(t2.left); // could be null
+    stack.push(t1.left); stack.push(t2.right);
+  }
+  
+  return true;
 }
 }
