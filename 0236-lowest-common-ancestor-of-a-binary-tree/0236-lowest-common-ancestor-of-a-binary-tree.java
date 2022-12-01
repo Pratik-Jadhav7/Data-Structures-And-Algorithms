34ffca9e8ff1f0ffe1dc52409ecf27a1d611923c
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-01 20:02:06 +0530

    Time: 25 ms (13.45%), Space: 49.7 MB (5.36%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,21 +8,28 @@
  * }
  */
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q){
-            return root;
-        }
-        TreeNode left =  lowestCommonAncestor(root.left, p, q);
-        TreeNode right =  lowestCommonAncestor(root.right, p, q);
-        
-        if(left == null){
-            return right;
-        }
-        else if(right == null){
-            return left;
-        }
-        else {
-            return root;
+   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    Map<TreeNode, TreeNode> parent = new HashMap<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    parent.put(root, null);
+    queue.add(root);
+    while (!parent.containsKey(p) || !parent.containsKey(q)) {
+        TreeNode node = queue.poll();
+        if (node != null) {
+            parent.put(node.left, node);
+            parent.put(node.right, node);
+            queue.add(node.left);
+            queue.add(node.right);
         }
     }
+    Set<TreeNode> set = new HashSet<>();
+    while (p != null) {
+        set.add(p);
+        p = parent.get(p);
+    }
+    while (!set.contains(q)) {
+        q = parent.get(q);
+    }
+    return q;
+}
 }
