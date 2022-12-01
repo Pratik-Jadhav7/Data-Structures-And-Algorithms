81854591827fe9ec8f9cceca44740a126e98069f
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-01 20:05:23 +0530

    Time: 12 ms (70.37%), Space: 46.6 MB (78.45%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -5,31 +5,24 @@
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
- * }
+ * 
  */
 class Solution {
-   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    Map<TreeNode, TreeNode> parent = new HashMap<>();
-    Queue<TreeNode> queue = new LinkedList<>();
-    parent.put(root, null);
-    queue.add(root);
-    while (!parent.containsKey(p) || !parent.containsKey(q)) {
-        TreeNode node = queue.poll();
-        if (node != null) {
-            parent.put(node.left, node);
-            parent.put(node.right, node);
-            queue.add(node.left);
-            queue.add(node.right);
-        }
-    }
-    Set<TreeNode> set = new HashSet<>();
-    while (p != null) {
-        set.add(p);
-        p = parent.get(p);
-    }
-    while (!set.contains(q)) {
-        q = parent.get(q);
-    }
-    return q;
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+  if (root == null || root == p || root == q)
+    // found p or q or touch the ground
+    return root;
+        
+  // search p and q from left and right
+  TreeNode left = lowestCommonAncestor(root.left, p, q);
+  TreeNode right = lowestCommonAncestor(root.right, p, q);
+  
+  if (left != null && right != null)
+    // from root's left & right we found both p and q, so root is the LCA
+    return root;
+  else
+    // left is not null means from left's left & right we found both q and q
+    // so left is the LCA, otherwise, right is the answer
+    return left != null ? left : right;
 }
 }
