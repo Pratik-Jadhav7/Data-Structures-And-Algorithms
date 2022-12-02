8ffef029a22f8468d36d95a3ed3b6f2f016754dc
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-02 23:10:00 +0530

    Time: 0 ms (100.00%), Space: 54 MB (65.94%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -15,16 +15,14 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null){
-            return new TreeNode(val);
-        }
-        if(root.val < val){
-            root.right = insertIntoBST(root.right, val);
-        }
-        else {
-            root.left = insertIntoBST(root.left, val);
-        }
+//      Lets see the Recursive Way
+        
+        if(root == null) return new TreeNode(val);
+        
+        if(root.val > val) root.left = insertIntoBST(root.left, val);
+        
+        else root.right = insertIntoBST(root.right, val);
+        
         return root;
     }
-    
 }
