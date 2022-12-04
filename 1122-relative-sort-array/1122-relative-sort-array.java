2be7949f664e110900a9cd7dea1fbba33b583394
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-04 22:55:36 +0530

    Time: 6 ms (40.43%), Space: 42.6 MB (61.70%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int[] cnt = new int[1001];
-        for(int n : arr1) cnt[n]++;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int n : arr1) map.put(n, map.getOrDefault(n, 0) + 1);
         int i = 0;
         for(int n : arr2) {
-            while(cnt[n]-- > 0) {
+            for(int j = 0; j < map.get(n); j++) {
                 arr1[i++] = n;
             }
+            map.remove(n);
         }
-        for(int n = 0; n < cnt.length; n++) {
-            while(cnt[n]-- > 0) {
+        for(int n : map.keySet()){
+            for(int j = 0; j < map.get(n); j++) {
                 arr1[i++] = n;
             }
         }
