245c16a020eeff034a5797f42c6716baad72b7cd
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-24 12:18:16 +0530

    Time: 1 ms (71.15%), Space: 40.3 MB (92.49%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,15 +14,26 @@
  * }
  */
 class Solution {
-       public TreeNode bstFromPreorder(int[] A) {
-        return bstFromPreorder(A, Integer.MAX_VALUE, new int[]{0});
-    }
-
-    public TreeNode bstFromPreorder(int[] A, int bound, int[] i) {
-        if (i[0] == A.length || A[i[0]] > bound) return null;
-        TreeNode root = new TreeNode(A[i[0]++]);
-        root.left = bstFromPreorder(A, root.val, i);
-        root.right = bstFromPreorder(A, bound, i);
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        stack.push(root);
+        for (int i = 1; i < preorder.length; i++) {
+            TreeNode node = new TreeNode(preorder[i]);
+            if (preorder[i] < stack.peek().val) {                
+                stack.peek().left = node;                
+            } else {
+                TreeNode parent = stack.peek();
+                while (!stack.isEmpty() && preorder[i] > stack.peek().val) {
+                    parent = stack.pop();
+                }
+                parent.right = node;
+            }
+            stack.push(node);            
+        }
         return root;
     }
 }
