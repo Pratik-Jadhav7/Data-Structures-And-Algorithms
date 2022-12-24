7fe0960b2794430b3e75fcfd0dd5aedcd3ce010e
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-24 12:15:27 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (78.11%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,16 +14,15 @@
  * }
  */
 class Solution {
-        int i = 0;
-    public TreeNode bstFromPreorder(int[] A) {
-        return bstFromPreorder(A, Integer.MAX_VALUE);
+       public TreeNode bstFromPreorder(int[] A) {
+        return bstFromPreorder(A, Integer.MAX_VALUE, new int[]{0});
     }
 
-    public TreeNode bstFromPreorder(int[] A, int bound) {
-        if (i == A.length || A[i] > bound) return null;
-        TreeNode root = new TreeNode(A[i++]);
-        root.left = bstFromPreorder(A, root.val);
-        root.right = bstFromPreorder(A, bound);
+    public TreeNode bstFromPreorder(int[] A, int bound, int[] i) {
+        if (i[0] == A.length || A[i[0]] > bound) return null;
+        TreeNode root = new TreeNode(A[i[0]++]);
+        root.left = bstFromPreorder(A, root.val, i);
+        root.right = bstFromPreorder(A, bound, i);
         return root;
     }
 }
