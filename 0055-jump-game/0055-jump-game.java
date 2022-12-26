e53aea4e5982286909022bbeb8e8b960183a754a
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-26 23:55:41 +0530

    Time: 2 ms (92.42%), Space: 42.8 MB (85.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,11 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-    int reachable = 0;
-    for (int i=0; i<nums.length; ++i) {
-        if (i > reachable) return false;
-        reachable = Math.max(reachable, i + nums[i]);
+    public boolean canJump(int[] A) {
+    int max = 0;
+    for(int i=0;i<A.length;i++){
+        if(i>max) {return false;}
+        max = Math.max(A[i]+i,max);
     }
     return true;
 }
+
 }
