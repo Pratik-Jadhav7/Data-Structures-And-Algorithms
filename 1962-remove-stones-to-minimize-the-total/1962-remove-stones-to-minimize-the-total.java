70bfe534f7fc01a1864d94ed440ee2029edf5af2
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-28 23:31:19 +0530

    Time: 637 ms (73.04%), Space: 57.1 MB (83.70%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,22 +1,34 @@
+// Algorithm:
+
+// Think Greedily – if we remove stones from the biggest piles first, we will get the optimal result, so the data structure that comes to mind is Max Heap.
+
+// Create a max heap.
+// Push all the piles [i] into the heap.
+// Iterate until k > 0.
+// Pop from the heap and decrement the value by floor (val / 2) and push the resultant value back in to the heap.
+// Finally, pop all the remaining elements in the heap and total them and return the sum.
 class Solution {
     public int minStoneSum(int[] piles, int k) {
-        int[] frequency = new int[10001];
-        int max = -1;
-        for (int p : piles) {
-            frequency[p]++;
-            max = Math.max(max, p);
+        Queue <Integer> heap = new PriorityQueue (new Comparator <Integer> () {
+           public int compare (Integer a, Integer b) {
+               if (a < b)
+                   return 1;
+               else if (a > b)
+                   return -1;
+               else
+                   return 0;
+           } 
+        });
+        for (int val : piles)
+            heap.offer (val);
+        while (k-- > 0) {
+            int stones = heap.poll ();
+            stones -= (int) (Math.floor (stones / 2));
+            heap.offer (stones);
         }
-        for (int i = max; i >= 1 && k > 0; i--) {
-            while(frequency[i] != 0 && k > 0) {
-                frequency[i]--;
-                frequency[(i+1)/2]++;
-                k--;
-            }
-        }
-        int ans = 0;
-        for (int i = max; i > 0; i--) {
-            ans += frequency[i] * i;
-        }
-        return ans;
+        int sum = 0;
+        while (!heap.isEmpty ())
+            sum += heap.poll ();
+        return sum;
     }
 }
