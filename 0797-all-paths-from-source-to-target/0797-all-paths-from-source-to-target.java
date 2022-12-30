4d0ecfbf845b6f14ef44c3d9e3d8264a68d04e10
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-12-30 23:56:17 +0530

    Time: 4 ms (69.42%), Space: 46.2 MB (67.71%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,26 +1,32 @@
-
-
 class Solution {
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-					
-        path.add(0);
-        dfsSearch(graph, 0, res, path);
-					
-        return res;
+public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+    Map<Integer, List<List<Integer>>> map = new HashMap<>();
+
+    return dfsSearch(graph, 0, map);   
+}
+
+private List<List<Integer>> dfsSearch(int[][] graph, int node, Map<Integer, List<List<Integer>>> map) {
+    if (map.containsKey(node)) {
+        return map.get(node);
     }
 
-    private void dfsSearch(int[][] graph, int node, List<List<Integer>> res, List<Integer> path) {
-        if (node == graph.length - 1) {
-            res.add(new ArrayList<Integer>(path));
-            return;
-        }
-
+    List<List<Integer>> res = new ArrayList<>();
+    if (node == graph.length - 1) {
+        List<Integer> path = new LinkedList<>();
+        path.add(node);
+        res.add(path);
+    } else {
         for (int nextNode : graph[node]) {
-            path.add(nextNode);
-            dfsSearch(graph, nextNode, res, path);
-            path.remove(path.size() - 1);
+            List<List<Integer>> subPaths = dfsSearch(graph, nextNode, map);
+            for (List<Integer> path : subPaths) {
+                LinkedList<Integer> newPath = new LinkedList<>(path);
+                newPath.addFirst(node);
+                res.add(newPath);
+            }
         }
     }
+
+    map.put(node, res);
+    return res;
+}
 }
