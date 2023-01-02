4e20c000dec12a88ec70487bd6fec2d3ea045866
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2023-01-02 23:36:05 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (87.47%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,5 +1,5 @@
 class Solution {
-   public boolean detectCapitalUse(String word) {
-    return word.matches("[A-Z]+|[a-z]+|[A-Z][a-z]+");
-}
+ public boolean detectCapitalUse(String word) {;
+        return word.equals(word.toLowerCase())||word.equals(word.toUpperCase())||word.substring(1).equals(word.substring(1).toLowerCase());
+    }
 }
